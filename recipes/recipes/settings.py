0419b94261c067566955fa2abe892ca57a8c35cf
Srishti Ahirwar <ahirwar.s@northeastern.edu>
2024-07-18 20:20:25 -0400

    final changes before deployment

--- recipes/recipes/settings.py
+++ recipes/recipes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)wf8r-h$qj9zbrrk2n+=_)-&$m)edgd704g*c=@&m4)=bz+bjz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['18.204.212.27']
 
 
 # Application definition
@@ -130,4 +130,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "http://roulettech-srishti.s3-website-us-east-1.amazonaws.com"
 ]
